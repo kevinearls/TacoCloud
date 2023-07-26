d2bea798cb898c456b15d9a9c2266fd9a4c070db
Kevin Earls <kevinearls@mac.com>
2023-07-26 15:57:21 +0200

    End of chapter1
    Signed-off-by: Kevin Earls <kevinearls@mac.com>

--- src/main/java/dev/kearls/tacocloud2/TacoCloud2Application.java
+++ src/main/java/dev/kearls/tacocloud2/TacoCloud2Application.java
@@ -1,12 +1,15 @@
 package dev.kearls.tacocloud2;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class TacoCloud2Application {
 
     public static void main(String[] args) {
+        log.info(">>>>>>>>> Starting now <<<<<<<<<<");
         SpringApplication.run(TacoCloud2Application.class, args);
     }
 
